fdcd51fa0f8592a9981d6c54389f7b4200ed36fa
Sai Ram Gubbala <67894080+sairam422@users.noreply.github.com>
2025-09-05 05:21:58 +0530

    day1-HomeAssignment

---
+++ primitive.js
@@ -0,0 +1,23 @@
+const city = "Vijayawada"
+console.log("I live in " + city)
+
+let marks = 85
+console.log(marks + 10)
+
+let isWeekend = true
+if (isWeekend) {
+    console.log("Relax")
+} else {
+    console.log("Work")
+}
+
+var futureGoal
+console.log(futureGoal)
+
+
+console.log("Value: " + city + " and it's type: " + typeof city)
+console.log("Value: " + marks + " and it's type: " + typeof marks)
+console.log("Value: " + isWeekend + " and it's type: " + typeof isWeekend)
+console.log("Value: " + futureGoal + " and it's type: " + typeof futureGoal)
+
+// The difference between let and var is in the scope of the variables they create: Variables declared by let are only available inside the block where they're defined. Variables declared by var are available throughout the function in which they're declared.
